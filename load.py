7e183a016bc562e3592993b9a5c77f8b0cb718dd
David McClure <davidwilliammcclure@gmail.com>
2022-06-30 23:28:38 -0400

    Blocking in the dataset streaming + stacking.

--- load.py
+++ load.py
@@ -1,11 +1,10 @@
 from pyspark.sql import SparkSession
 from huggingface_hub import HfApi
 from datasets import load_dataset
+from itertools import islice
 
 
-def main():
-    spark = SparkSession.builder.getOrCreate()
-
+def list_lm_dataset_ids():
     api = HfApi()
 
     # TODO: How to inject the token in prod?
@@ -15,11 +14,23 @@ def main():
         use_auth_token=True,
     )
 
-    print(len(datasets))
+    return [ds.id for ds in datasets if 'cleaned_lm' in ds.id]
 
-    loam_datasets = [ds for ds in datasets if 'cleaned_lm' in ds.id]
 
-    print(len(loam_datasets))
+def iter_dataset(dataset_id: str):
+    ds = load_dataset(dataset_id, use_auth_token=True, streaming=True)
+    yield from iter(ds)
+
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+
+    ids = list_lm_dataset_ids()
+    ids = spark.sparkContext.parallelize(ids)
+
+    rows = ids.flatMap(lambda ds_id: islice(iter_dataset(ds_id), 100))
+
+    return rows
 
 
 if __name__ == '__main__':
